97a192d1bfa784344a90474e81687e98ef4fa38c
HentaiSamurai <togearisubesubetogetoge@gmail.com>
2024-07-24 01:16:27 +0900

    ntp問い合わせ結果をそのままログに入れる方式に変更

--- sync_timestamp.sh
+++ sync_timestamp.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# NTP サーバと時刻同期
-ntpdate -q  ntp.nict.jp
+# ntpサーバへの問い合わせ記録を書き込む
+ntpdate -q  ntp.nict.jp | tee sync_log.txt
 #予備、国立天文台
-#ntpdate ntp.nict.jp
-
-# 同期日時をログファイルに記録
-rm sync_log.txt
-sync_time=$(date)
-echo "Sync Time: $sync_time" >> sync_log.txt
+#ntpdate -q  s2csntp.miz.nao.ac.jp | tee sync_log.txt
 
 # コミットメッセージに同期日時を追加
-#git add sync_log.txt
+git add sync_log.txt
 #git commit -m "Sync Timestamp: $sync_time"
